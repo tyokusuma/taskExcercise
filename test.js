4a5e00860e576bfcac52c15e70d4b9a369e5ebfe
tyokusuma <tyoaris21@gmail.com>
2017-04-25 15:48:23 +0700

    prettier number

---
+++ test.js
@@ -0,0 +1,27 @@
+// function loop() {
+//   let i=0;
+//   while (i<=10) {
+//     console.log(i);
+//     i++;
+//
+//   }
+// }
+// console.log(loop());
+
+
+function fibonacci(n) {
+    for (var i = 0; i < n; i++) {
+  }
+}
+console.log(fibonacci(10));
+
+// function loop {
+// var points = new Array(100);
+//         for (var i = 0; i < 100; i++) {
+//             points[i] = i + 1; //This populates the array.  +1 is necessary because arrays are 0 index based and you want to store 1-100 in it, NOT 0-99.
+//         }
+//
+//         for (var i = 0; i < points.length; i++) {
+//             console.log(points[i]); //This prints the values that you stored in the array
+//         }
+// }
